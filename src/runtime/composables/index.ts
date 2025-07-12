3fd1456931f0928f6190af8fb550551d569d9a36
Peter Hoffman <peter@huestudios.com>
2025-07-12 10:55:59 -0400

    Duplicate imports.

--- src/runtime/composables/index.ts
+++
@@ -1,7 +0,0 @@
-// src/runtime/composables/index.ts
-// Only export composables, not types (to avoid duplicates)
-export { useDirectusNewsletter } from './useDirectusNewsletter'
-export { useMjmlCompiler } from './useMjmlCompiler'
-export { useNewsletter } from './useNewsletter'
-export { useNewsletterEditor } from './useNewsletterEditor'
-export { useSendGrid } from './useSendGrid'
